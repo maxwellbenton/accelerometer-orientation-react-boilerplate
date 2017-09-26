030bd2527a5bfd03b461d41758b1c4e698c8c045
Maxwell Benton <maxwellbenton@gmail.com>
2017-09-25 22:55:55 -0400

    working on overlay

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom'
 import Navigation from './Navigation'
 import GameBoard from './GameBoard'
 import PlayerView from './PlayerView'
@@ -19,21 +20,33 @@ class App extends Component {
         }
         return b
       })(),
+      prevBoard: (() => {
+
+        let b = new Array(10)
+        for(let m = 0; m < 10; m++) {
+          b[m] = new Array(10)
+          for(let n = 0; n < 10; n++) {
+            b[m][n] = {fill: "#EEE", owner: "none"}
+          }
+        }
+        return b
+      })(),
+      previewDivs: null,
       pieces: (() => {
         let b = []
         b.push({shape:[[0,0]], type: "house"})
         b.push({shape:[[0,0]], type: "house2"})
-        b.push({shape: [[0,0],[1,0]], type: "aparments"})
-        b.push({shape: [[0,0],[1,0]], type: "aparments2"})
+        b.push({shape:[[0,0],[1,0]], type: "aparments"})
+        b.push({shape:[[0,0],[1,0]], type: "aparments2"})
         b.push({shape:[[0,0],[0,1],[1,0],[1,1]], type: "square"})
-        b.push({shape:[[-1,0],[0,0],[1,0]], type: "road"})
-        b.push({shape:[[-1,0],[0,0],[1,0]], type: "road2"})
-        b.push({shape:[[-1,0],[0,0],[1,0],[0,1]], type: "school"})
-        b.push({shape:[[-1,0],[0,0],[1,0],[-1,1],[1,1]], type: "mansion"})
-        b.push({shape:[[0,-1],[-1,0],[0,0],[1,0],[0,1]], type: "hospital"})
-        b.push({shape:[[1,-1],[-1,0],[0,0],[1,0],[-1,1]], type: "office"})
-        b.push({shape:[[0,-1],[-1,0],[0,0],[1,0],[-1,1]], type: "federal"})
-        b.push({shape:[[0,-1],[1,-1],[-1,0],[0,0],[-1,1]], type: "tenements"})
+        b.push({shape:[[0,0],[-1,0],[1,0]], type: "road"})
+        b.push({shape:[[0,0],[-1,0],[1,0]], type: "road2"})
+        b.push({shape:[[0,0],[-1,0],[1,0],[0,1]], type: "school"})
+        b.push({shape:[[0,0],[-1,0],[1,0],[-1,1],[1,1]], type: "mansion"})
+        b.push({shape:[[0,0],[0,-1],[-1,0],[1,0],[0,1]], type: "hospital"})
+        b.push({shape:[[0,0],[1,-1],[-1,0],[1,0],[-1,1]], type: "office"})
+        b.push({shape:[[0,0],[0,-1],[-1,0],[1,0],[-1,1]], type: "federal"})
+        b.push({shape:[[0,0],[0,-1],[1,-1],[-1,0],[-1,1]], type: "tenements"})
         return b
       })(),
       selectedPiece: null,
@@ -41,12 +54,15 @@ class App extends Component {
     }
     this.selectPiece = this.selectPiece.bind(this)
     this.rotatePiece = this.rotatePiece.bind(this)
+    this.prieviewPlacement = this.prieviewPlacement.bind(this)
+    this.clearPreview = this.clearPreview.bind(this)
     this.placePiece = this.placePiece.bind(this)
+    this.orientPiece = this.orientPiece.bind(this)
   }
 
-  selectPiece(piece) {
-    this.setState(() => {
-      return {selectedPiece: piece}
+  selectPiece(nPiece) {
+    this.setState((prevState) => {
+      return {selectedPiece: prevState.pieces.filter(piece => piece.type === nPiece)[0]}
     })
   }
 
@@ -57,13 +73,80 @@ class App extends Component {
       })
     }
   }
+  orientPiece() {
+    if(this.state.selectedPiece !== null) {
+    var orientedPiece = this.state.selectedPiece
+    for(let k = 0; k < this.state.orientation; k++) {
+    	orientedPiece = {shape: orientedPiece.shape.map(spot => [spot[1],-1*spot[0]]), type: this.state.selectedPiece.type}
+    }
+    return orientedPiece
+    } else {
+      return null
+    }
+  }
+
+  prieviewPlacement(piece, position) {
+    let b = this.state.board
+    let pb = this.state.board
+    let errors = false;
+    let overlayPiece = piece
+    debugger
+    for(let k = 0; k < this.state.orientation; k++) {
+    	piece.shape = piece.shape.map(spot => [spot[1],-1*spot[0]])
+    }
+    for(let p = 0; p < piece.shape.length; p++) {
+        if(b[position[0]+piece.shape[p][0]] && b[position[0]+piece.shape[p][0]][position[1]+piece.shape[p][1]] && b[position[0]+piece.shape[p][0]][position[1]+piece.shape[p][1]].fill === "#EEE") {
+          continue;
+        } else {
+          errors = true;
+        }
+    }
+
+    if(!errors) {
+      for(let p = 0; p < piece.shape.length; p++) {
+          b[position[0]+piece.shape[p][0]][position[1]+piece.shape[p][1]].fill = "#FFF"
+      }
+    }
+
+    this.setState((prevState, curProps) => {
+      debugger
+      return {
+        board: b
+      }
+    })
+  }
+
+  clearPreview() {
+    // let b = this.state.board
+    // let pb = this.state.prevBoard
+    // let errors = false;
+    // for(let k = 0; k < this.state.orientation; k++) {
+    // 	piece.shape = piece.shape.map(spot => [spot[1],-1*spot[0]])
+    // }
+    // for(let p = 0; p < piece.shape.length; p++) {
+    //     if(b[position[0]+piece.shape[p][0]] && b[position[0]+piece.shape[p][0]][position[1]+piece.shape[p][1]] && b[position[0]+piece.shape[p][0]][position[1]+piece.shape[p][1]].fill === "#EEE") {
+    //       continue;
+    //     } else {
+    //       errors = true;
+    //     }
+    // }
+
+    // if(!errors) {
+    //   for(let p = 0; p < piece.shape.length; p++) {
+    //       b[position[0]+piece.shape[p][0]][position[1]+piece.shape[p][1]].fill = pb[position[0]+piece.shape[p][0]][position[1]+piece.shape[p][1]].fill
+    //   }
+    // }
+    this.setState((prevState, curProps) => {
+      return {
+        board: prevState.prevBoard
+      }
+    })
+  }
 
   placePiece(piece, position) {
     let b = this.state.board
     let errors = false;
-    for(let k = 0; k < this.state.orientation; k++) {
-    	piece.shape = piece.shape.map(spot => [spot[1],-1*spot[0]])
-    }
+    
     for(let p = 0; p < piece.shape.length; p++) {
         if(b[position[0]+piece.shape[p][0]] && b[position[0]+piece.shape[p][0]][position[1]+piece.shape[p][1]] && b[position[0]+piece.shape[p][0]][position[1]+piece.shape[p][1]].fill === "#EEE") {
           continue;
@@ -88,12 +171,14 @@ class App extends Component {
     })
   }
 
+  
+
   render() {
     return (
       <div className="App">
         <Navigation />
         <div className="GameWrapper">
-          <GameBoard board={this.state.board} pieces={this.state.pieces} selectedPiece={this.state.selectedPiece} placePiece={this.placePiece}/>
+          <GameBoard board={this.state.board} pieces={this.state.pieces} selectedPiece={this.orientPiece()} placePiece={this.placePiece} prieviewPlacement={this.prieviewPlacement} clearPreview={this.clearPreview} orientation={this.state.orientation}/>
         </div>
         <PlayerView pieces={this.state.pieces} selectPiece={this.selectPiece} rotatePiece={this.rotatePiece}/>
       </div>
--- src/GameBoard.js
+++ src/GameBoard.js
@@ -3,35 +3,88 @@ import React, { Component } from 'react';
 class GameBoard extends Component {
   constructor () {
     super()
+    this.state = {
+      overlayDivs: null,
+      validMove: null
+    }
     this.handleClick = this.handleClick.bind(this)
+    this.handleMouseOver = this.handleMouseOver.bind(this)
+    this.handleMouseOut = this.handleMouseOut.bind(this)
+    this.renderOverlay = this.renderOverlay.bind(this)
   }
 
   handleClick(e) {
-    // e.target.style.backgroundColor === "red" ? e.target.style.backgroundColor = "grey" : e.target.style.backgroundColor = "red"
     if(this.props.selectedPiece !== null) {
-      let p = this.props.pieces.filter(piece => piece.type === this.props.selectedPiece)[0]
       let pos = e.target.id.split(',').map(p => parseInt(p))
-      this.props.placePiece(p, pos)
+      this.props.placePiece(this.props.selectedPiece, pos)
     }
+  }
+
+  handleMouseOver(e) {
+    if(this.props.selectedPiece !== null) {
+      let pos = e.target.id.split(',').map(p => parseInt(p))
+      let posArray = this.props.selectedPiece.shape.map(spot => [pos[0]+spot[0],pos[1]+spot[1]])
+      let newOverlay = []
+      for(let p = 0; p < posArray.length; p++) {
+        if(pos[0]+this.props.selectedPiece.shape[p][0] < 10 && pos[0]+this.props.selectedPiece.shape[p][0] > -1 && pos[1]+this.props.selectedPiece.shape[p][1] < 10 && pos[1]+this.props.selectedPiece.shape[p][1] > -1) {
+          newOverlay.push([pos[0]+this.props.selectedPiece.shape[p][0], pos[1]+this.props.selectedPiece.shape[p][1]])
+        } else {
+          continue;
+        }
+      }
+      if(newOverlay.length === posArray.length) {
+        this.setState(() => {
+          return {overlayDivs: {tiles: newOverlay, validMove: true}}
+        })
+      } else {
+        this.setState(() => {
+          return {overlayDivs: {tiles: newOverlay, validMove: false}}
+        })
+      }
+    }
+  }
+  handleMouseOut(e) {
+    debugger
+    this.handleMouseOver(e)
   }
 
 
   renderBoard() {
     let nb = []
+  
     this.props.board.forEach((row,m) => {
       row.forEach((tile,n) => {
-        nb.push(<div className="GameTile" id={`${m},${n}`} style={{top:m*50, left: n*50, backgroundColor: tile.fill }} onClick={this.handleClick}/>)
+        nb.push(<div className="GameTile" key={`${m},${n}`} id={`${m},${n}`} style={{top:m*50, left: n*50, backgroundColor: tile.fill }} onClick={this.handleClick} onMouseOver={this.handleMouseOver}/>)
       })
     })
     return nb
   }
 
+  renderOverlay() {
+    if(this.props.selectedPiece !== null && this.state.overlayDivs !== null) {
+      
+      var newOverlay = []
+      if(this.state.overlayDivs.validMove) {
+        this.state.overlayDivs.tiles.forEach((div) => {
+        newOverlay.push(<div className="OverlayTile" key={`o${div[0]},${div[1]}`} id={`o${div[0]},${div[1]}`} style={{top:div[0]*50, left: div[1]*50, backgroundColor: 'white' }} onMouseLeave={this.handleMouseOver}/>)
+        })
+      } else {
+        debugger
+        newOverlay.push(<div className="OverlayTile" key={`o${this.state.overlayDivs.tiles[0][0]},${this.state.overlayDivs.tiles[0][1]}`} id={`o${this.state.overlayDivs.tiles[0][0]},${this.state.overlayDivs.tiles[0][1]}`} style={{top:this.state.overlayDivs.tiles[0][0]*50, left: this.state.overlayDivs.tiles[0][1]*50, backgroundColor: 'red' }} onMouseLeave={this.handleMouseOver}/>)
+      }
+      console.log(newOverlay)
+      return newOverlay
+    }
+  }
 
   render() {
     return (
-      <div className="GameBoard">
-        {this.renderBoard()}
-      </div>
+      //<div className="boardWrapper">
+        <div className="GameBoard">
+          {this.renderBoard()}
+          {this.renderOverlay()}
+        </div>
+      //</div>
     );
   }
 }
