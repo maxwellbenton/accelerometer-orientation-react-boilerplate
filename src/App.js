472d070a987a6f4639667bdfcea9bf8ebcee76ef
maxwell <github email address>
2017-11-21 17:52:57 -0500

    angles angels engels communism

--- src/App.js
+++ src/App.js
@@ -16,36 +16,102 @@ class App extends Component {
       beta: 0,
       gamma: 0
     },
+    rotationInMotion: {
+      alpha: 0,
+      beta: 0,
+      gamma: 0
+    },
+    acceleration: {
+      x: 0,
+      y: 0,
+      z: 0
+    },
+    accelerationWithGravity: {
+      x: 0,
+      y: 0,
+      z: 0
+    },
     north: {
       alpha: 0,
       beta: 0,
       gamma: 0
     },
-    score: 0,
-    timer: 60,
-    point: { a: 0, b: 0, g: 0 },
-    loading: null,
-    gameStarted: false
+    origin: {
+      x: 0,
+      y: 0,
+      z: 0,
+      a: 0,
+      b: 0,
+      g: 0
+    },
+    distanceTest: {
+      list: []
+    }
   };
 
   componentDidMount() {
     if (window.DeviceOrientationEvent) {
       window.addEventListener("devicemotion", this.deviceMotionListener);
+      window.addEventListener(
+        "deviceorientation",
+        this.deviceOrientationListener
+      );
+      setInterval(() => {
+        this.updateOrigin();
+      }, 1000 / 30);
     } else {
       alert("Sorry, your browser doesn't support Device Orientation");
     }
   }
 
-  deviceMotionListener = event => {
+  deviceOrientationListener = event => {
     this.setState({
       rotation: {
-        alpha: 50, //Math.round(event.rotationRate.alpha),
-        beta: 60, //Math.round(event.rotationRate.beta),
-        gamma: 0 //Math.round(event.rotationRate.gamma)
+        alpha: Math.round(event.alpha),
+        beta: Math.round(event.beta),
+        gamma: Math.round(event.gamma)
       }
     });
   };
 
+  deviceMotionListener = event => {
+    this.setState(pState => ({
+      acceleration: {
+        x: event.acceleration.x < 0.1 ? 0 : event.acceleration.x,
+        y: event.acceleration.y < 0.1 ? 0 : event.acceleration.y,
+        z: event.acceleration.z < 0.1 ? 0 : event.acceleration.z
+      },
+      accelerationWithGravity: {
+        x: Math.round(event.accelerationIncludingGravity.x * 10) / 10,
+        y: Math.round(event.accelerationIncludingGravity.y * 10) / 10,
+        z: Math.round(event.accelerationIncludingGravity.z * 10) / 10
+      },
+      rotationInMotion: {
+        alpha: Math.round(event.rotationRate.alpha),
+        beta: Math.round(event.rotationRate.beta),
+        gamma: Math.round(event.rotationRate.gamma)
+      }
+    }));
+  };
+
+  updateOrigin = () => {
+    this.setState(pState => {
+      return {
+        origin: {
+          ...pState.origin,
+          x: 0, //pState.origin.x - pState.acceleration.x,
+          y: 0, //pState.origin.y - pState.acceleration.y,
+          z: 0, //pState.origin.z - pState.acceleration.z
+          a: 0,
+          b: 0,
+          g: 0
+        },
+        distanceTest: {
+          list: [...pState.distanceTest.list]
+        }
+      };
+    });
+  };
   render() {
     return (
       <div className="App">
--- src/GameBoard.js
+++ src/GameBoard.js
@@ -7,19 +7,94 @@ class GameBoard extends Component {
         <div
           className="BoardLayer"
           style={{
-            width: "100%",
-            height: "100%",
-            backgroundColor: "#eee",
-            transform: `rotateX(${
-              this.props.rotation.beta
-            }deg) translateZ(-50px)`
+            backgroundColor: "#E33",
+            transform: `translateX(${this.props.origin.x}px) translateY(${
+              this.props.origin.y
+            }px) translateZ(${this.props.origin.z}px) rotateX(${
+              this.props.origin.b
+            }deg) rotateY(${this.props.origin.g}deg) rotateZ(${
+              this.props.origin.a
+            }deg)`
           }}
         >
-          Hey
+          <div
+            style={{
+              width: "50%",
+              height: "50%",
+              position: "absolute",
+              top: 0,
+              left: 0
+            }}
+          >
+            <h3>A: {this.props.rotation.alpha}</h3>
+            <h3>B: {this.props.rotation.beta}</h3>
+            <h3>G: {this.props.rotation.gamma}</h3>
+          </div>
+          <div
+            style={{
+              width: "50%",
+              height: "50%",
+              position: "absolute",
+              top: 0,
+              left: "50%"
+            }}
+          >
+            <h3>A: {this.props.rotationInMotion.alpha}</h3>
+            <h3>B: {this.props.rotationInMotion.beta}</h3>
+            <h3>G: {this.props.rotationInMotion.gamma}</h3>
+          </div>
+          <div
+            style={{
+              width: "50%",
+              height: "50%",
+              position: "absolute",
+              top: "50%",
+              left: 0
+            }}
+          >
+            <h3>X: {Math.round(this.props.acceleration.x)}</h3>
+            <h3>Y: {Math.round(this.props.acceleration.y)}</h3>
+            <h3>Z: {Math.round(this.props.acceleration.z)}</h3>
+          </div>
+          <div
+            style={{
+              width: "50%",
+              height: "50%",
+              position: "absolute",
+              top: "50%",
+              left: "50%"
+            }}
+          >
+            <h3>X: {this.props.accelerationWithGravity.x}</h3>
+            <h3>Y: {this.props.accelerationWithGravity.y}</h3>
+            <h3>Z: {this.props.accelerationWithGravity.z}</h3>
+          </div>
+          <div
+            style={{
+              width: "50%",
+              height: "50%",
+              position: "absolute",
+              top: "25%",
+              left: "25%"
+            }}
+          >
+            <h3>X: {Math.round(this.props.origin.x)}</h3>
+            <h3>Y: {Math.round(this.props.origin.y)}</h3>
+            <h3>Z: {Math.round(this.props.origin.z)}</h3>
+          </div>
         </div>
       </div>
     );
   }
 }
+
+// style={{
+//   width: "100%",
+//   height: "100%",
+//   backgroundColor: "#eee",
+//   transform: `rotateX(${
+//     this.props.rotation.beta
+//   }deg) translateZ(-50px)`
+// }}
 
 export default GameBoard;
